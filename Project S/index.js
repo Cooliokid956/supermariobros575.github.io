2342bdc75acb42e6c64606e092f79215b2311062
supermariobros575 <75806962+supermariobros575@users.noreply.github.com>
2021-03-30 22:42:21 -0700

    Create index.js

---
+++ Project S/index.js
@@ -0,0 +1,14 @@
+﻿var time = Date.now();
+
+document.onreadystatechange = function (event) {
+    if (event.target.readyState !== "complete") {
+        return;
+    }
+
+    var UserWrapper = new UserWrappr.UserWrappr(Project S.Project S.prototype.proliferate(
+        {
+            "GameStartrConstructor": Project S.Project S
+        }, Project S.Project S.settings.ui, true));
+
+    console.log("It took " + (Date.now() - time) + " milliseconds to start."), UserWrapper.displayHelpMenu()
+};
