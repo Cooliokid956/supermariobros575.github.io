eb61ed189dd3441ae3c589c83ef63c7b3437cd8e
supermariobros575 <75806962+supermariobros575@users.noreply.github.com>
2021-03-31 23:18:25 -0700

    Update game.js

--- Super Mario Bros 575/js/game.js
+++ Super Mario Bros 575/js/game.js
@@ -54,7 +54,8 @@ function init() {
     overworld: new Audio('sounds/overworld.mp3'),
     underground: new Audio('sounds/underground_bgm.ogg'),
     clear: new Audio('sounds/stage_clear.wav'),
-    death: new Audio('sounds/Mario Death.wav')
+    death: new Audio('sounds/Mario Death.wav'),
+    star: new Audio('sounds/superstar.mp3')
   };
   sounds = {
     smallJump: new Audio('sounds/jump-small.wav'),
