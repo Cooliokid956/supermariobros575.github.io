00e4aad2954d6b36138e4cfa2316eb9f508a5cac
supermariobros575 <75806962+supermariobros575@users.noreply.github.com>
2021-04-03 21:20:12 -0700

    Update 11.js

--- Super Mario Bros 575/js/levels/11.js
+++ Super Mario Bros 575/js/levels/11.js
@@ -124,7 +124,11 @@ var oneone = Mario.oneone = function() {
   level.putQBlock(23, 9, new Mario.Bcoin([368, 144]));
   level.putBrick(24, 9, null);
   level.putRealPipe(34, 10, 3, "DOWN", Mario.oneonetunnel);
-  level.putWall(36, 10, 1);
+  level.putWall(36, 11, 1);
+  level.putWall(37, 11, 1);
+  level.putWall(38, 11, 1);
+  level.putWall(39, 11, 1);
+  level.putWall(40, 11, 1);
 
   //and enemies
   level.putGoomba(22, 12);
